894981b2b53ca05a44214e6543f229c5af90658a
artssz <artsoucor9@gmail.com>
2021-10-05 18:40:14 -0300

    cliente, pessoa

---
+++ src/classes/Cliente.java
@@ -0,0 +1,59 @@
+package classes;
+
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class Cliente extends Pessoa{
+    private ArrayList<Pedido> pedidos;
+    private boolean fazAniversario;
+    private boolean jaFezPedidoAniversario;
+
+    public Cliente(long cpf, String nome, String dataDeNascimento) {
+        super(cpf, nome, dataDeNascimento);
+        String[] data_nascimento = dataDeNascimento.split("/");
+        Calendar c = Calendar.getInstance();
+        for(int i = 0; i < data_nascimento.length; i++){
+        if(c.get(Calendar.DAY_OF_MONTH)==Integer.parseInt(data_nascimento[0]) && c.get(Calendar.MONTH) == Integer.parseInt(data_nascimento[1])){
+            fazAniversario = true;
+            
+        }else{
+            fazAniversario = false;
+        }
+        if(checarAniversario()){
+            
+            jaFezPedidoAniversario = true;
+        }else{
+            jaFezPedidoAniversario = false;
+            
+        }
+        }  
+    }
+    public void pagar(){
+        for(int i = 0; i < pedidos.size();i++){
+            if(pedidos.get(i).isPago() != true){
+                pedidos.get(i).setPago(true);
+            }
+    
+    }
+    }
+    public void zerarQuantidadeDePedidos(){
+        pedidos.clear();
+    
+    
+    }
+    public boolean checarAniversario(){
+        if(fazAniversario == true){
+        for(int i = 0; i < pedidos.size();i++){
+            if(pedidos.get(i).isDeAniversariante()){
+                return true;
+            }
+        }
+        return false;
+    }
+        return false;
+    }
+    
+    
+    
+}

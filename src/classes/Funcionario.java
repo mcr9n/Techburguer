5de0793e231627c901d041469c44a86d4c9afb28
Marcelo Piano <marcelopianopatuscosantiago@gmail.com>
2021-10-05 18:59:19 -0300

    Adiciona Classe Funcionario.

---
+++ src/classes/Funcionario.java
@@ -0,0 +1,30 @@
+package classes;
+
+public class Funcionario extends Pessoa {
+
+    private String senha;
+    private float salario;
+
+    public Funcionario(String senha, float salario, long cpf, String nome, String dataDeNascimento) {
+        super(cpf, nome, dataDeNascimento);
+        this.senha = senha;
+        this.salario = salario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+}

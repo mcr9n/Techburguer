b866172c0a6340dfebde8b96fadba1b61b9cc115
artssz <artsoucor9@gmail.com>
2021-10-05 18:28:53 -0300

    cliente, pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,40 @@
+
+public class Pessoa {
+    protected long cpf;
+    protected String nome;
+    protected String dataDeNascimento;
+
+    public Pessoa(long cpf, String nome, String dataDeNascimento) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.dataDeNascimento = dataDeNascimento;
+    }
+    
+    
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+    
+    
+}
